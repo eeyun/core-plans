74dca3a6492cfc8b890767acae24faa496ea66ff
Ian Henry <ihenry@chef.io>
2016-11-08 18:05:26 -0500

    hacket hack
    Signed-off-by: Ian Henry <ihenry@chef.io>

--- bin/travis.sh
+++ bin/travis.sh
@@ -10,6 +10,6 @@ pre-commit install
 
 # The TRAVIS_COMMIT env var is always a merge commit, so we want to test from
 # the original commit all the way up until HEAD.
-ORIGIN=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f1 -d'.')
+#ORIGIN=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f1 -d'.')
 
-pre-commit run --origin "$ORIGIN" --source HEAD
+pre-commit run #--origin "$ORIGIN" --source HEAD
